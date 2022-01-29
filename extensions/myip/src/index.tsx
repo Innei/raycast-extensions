56d6ff288d1a99cd1e7ba2da4f1a0190b111aa96
Innei <tukon479@gmail.com>
2022-01-29 12:21:54 +0800

    refactor: ip info

--- extensions/myip/src/index.tsx
+++ extensions/myip/src/index.tsx
@@ -1,61 +1,88 @@
 import {
   ActionPanel,
   ActionPanelItem,
-  copyTextToClipboard,
+  CopyToClipboardAction,
+  Icon,
   List,
   ListItem,
-  showHUD,
   useNavigation,
-} from "@raycast/api";
-import http from "http";
-import os from "os";
-import { FC, useEffect, useMemo, useState } from "react";
-const networkInterfaces = os.networkInterfaces();
+} from '@raycast/api'
+import http from 'http'
+import os from 'os'
+import { FC, useEffect, useMemo, useState } from 'react'
+const networkInterfaces = os.networkInterfaces()
 
-const networkEntires = Object.entries(networkInterfaces).filter(([k]) => !k.startsWith("lo"));
+const networkEntires = Object.entries(networkInterfaces).filter(
+  ([k]) => !k.startsWith('lo'),
+)
+
 const list = networkEntires
   .map(([k, v]) => {
+    console.log(
+      Array.from(
+        v
+          ?.flat()
+          .reduce((acc, { mac }) => acc.add(mac), new Set())
+          .values() || [],
+      ).join(', '),
+    )
+
     return {
       interface$: k,
       addresses: v?.flat().map(({ address }) => address),
-      isV4: v?.flat().some(({ family }) => family === "IPv4"),
-    };
+      isV4: v?.flat().some(({ family }) => family === 'IPv4'),
+      mac: Array.from(
+        v
+          ?.flat()
+          .reduce((acc, { mac }) => acc.add(mac), new Set())
+          .values() || [],
+      ).join(', '),
+    }
   })
-  .sort((a) => (a.interface$ === "en0" ? -1 : a.isV4 ? -1 : 1));
+  .filter((i) => i.mac !== '00:00:00:00:00:00')
+  .sort((a) => (a.interface$ === 'en0' ? -1 : a.isV4 ? -1 : 1))
 
 export type IpInfo = {
-  ip: string;
-  city: string;
-  region: string;
-  country: string;
-  loc: string;
-  org: string;
-  timezone: string;
-};
+  ip: string
+  city: string
+  region: string
+  country: string
+  loc: string
+  org: string
+  timezone: string
+}
 export default function main() {
-  const [publicIp, setPublicIp] = useState<string | undefined>(undefined);
-  const [ipInfo, setIpInfo] = useState<IpInfo | null>(null);
-  const [error, setError] = useState(false);
+  const [publicIp, setPublicIp] = useState<string | undefined>(undefined)
+  const [ipInfo, setIpInfo] = useState<IpInfo | null>(null)
+  const [error, setError] = useState(false)
   useEffect(() => {
-    fetchIp();
-  }, []);
+    fetchIp()
+  }, [])
 
   function fetchIp() {
-    setError(false);
-    http.get({ host: "ipinfo.io", port: 80, path: "/", headers: { "user-agent": "curl" } }, function (resp) {
-      resp.on("data", function (ip: Buffer) {
-        const data = JSON.parse(ip.toString("utf-8"));
-        setPublicIp(data.ip);
-        delete data.readme;
-        setIpInfo(data);
-      });
+    setError(false)
+    http.get(
+      {
+        host: 'ipinfo.io',
+        port: 80,
+        path: '/',
+        headers: { 'user-agent': 'curl' },
+      },
+      function (resp) {
+        resp.on('data', function (ip: Buffer) {
+          const data = JSON.parse(ip.toString('utf-8'))
+          setPublicIp(data.ip)
+          delete data.readme
+          setIpInfo(data)
+        })
 
-      resp.on("error", () => {
-        setError(true);
-      });
-    });
+        resp.on('error', () => {
+          setError(true)
+        })
+      },
+    )
   }
-  const nav = useNavigation();
+  const nav = useNavigation()
 
   const Detail = useMemo(
     () => (
@@ -68,7 +95,7 @@ export default function main() {
               accessoryTitle={k.charAt(0).toUpperCase() + k.slice(1)}
               actions={
                 <ActionPanel>
-                  <CopyAction value={v} />
+                  <CopyToClipboardAction content={v} />
                 </ActionPanel>
               }
             ></ListItem>
@@ -76,78 +103,87 @@ export default function main() {
         </List>
       </>
     ),
-    [ipInfo]
-  );
+    [ipInfo],
+  )
   return (
     <List isLoading={!publicIp}>
       <List.Item
-        title={error ? "Fetch Error, Retry Press Enter." : publicIp || "Fetching.."}
-        accessoryTitle={"Public IP"}
+        icon={Icon.Globe}
+        title={
+          error ? 'Fetch Error, Retry Press Enter.' : publicIp || 'Fetching..'
+        }
+        accessoryTitle={'Public IP'}
         actions={
           publicIp ? (
             <ActionPanel>
-              <CopyAction value={publicIp} />
+              <CopyToClipboardAction content={publicIp} />
               <ActionPanelItem
-                shortcut={{ key: "tab", modifiers: [] }}
+                shortcut={{ key: 'tab', modifiers: [] }}
                 title="Detail"
                 onAction={() => {
-                  nav.push(Detail);
+                  nav.push(Detail)
                 }}
               ></ActionPanelItem>
             </ActionPanel>
           ) : error ? (
             <ActionPanel>
-              <ActionPanelItem title="Retry" onAction={fetchIp}></ActionPanelItem>
+              <ActionPanelItem
+                title="Retry"
+                onAction={fetchIp}
+              ></ActionPanelItem>
             </ActionPanel>
           ) : null
         }
       />
-      {list.map(({ interface$, addresses }) => {
+      {list.map(({ interface$, addresses, mac }, idx) => {
         if (!addresses) {
-          return;
+          return null
         }
         if (addresses.length > 1) {
           return addresses.map((add) => {
             return !add ? null : (
-              <List.Item
-                actions={
-                  <ActionPanel>
-                    <CopyAction value={add} />
-                  </ActionPanel>
-                }
+              <IpItem
                 key={add}
-                title={add}
-                accessoryTitle={interface$}
-              ></List.Item>
-            );
-          });
+                mac={mac || ''}
+                interface$={interface$}
+                address={add}
+              ></IpItem>
+            )
+          })
         } else if (addresses[0]) {
           return (
-            <List.Item
-              actions={
-                <ActionPanel>
-                  <CopyAction value={addresses[0]} />
-                </ActionPanel>
-              }
-              key={interface$}
-              title={addresses[0]}
-              accessoryTitle={interface$}
-            ></List.Item>
-          );
-        } else return null;
+            <IpItem
+              key={idx}
+              mac={mac || ''}
+              interface$={interface$}
+              address={addresses[0]}
+            ></IpItem>
+          )
+        } else return null
       })}
     </List>
-  );
+  )
 }
 
-const CopyAction: FC<{ value: string }> = ({ value }) => {
+export const IpItem: FC<{
+  mac?: string
+  address?: string
+  interface$: string
+}> = (props) => {
+  const { mac, interface$, address } = props
   return (
-    <ActionPanelItem
-      title="Copy"
-      onAction={() => {
-        copyTextToClipboard(value);
-        showHUD("Copy Success");
-      }}
-    ></ActionPanelItem>
-  );
-};
+    <List.Item
+      icon={Icon.Circle}
+      subtitle={mac || ''}
+      actions={
+        <ActionPanel>
+          <CopyToClipboardAction content={address || ''} />
+          <CopyToClipboardAction title="Copy Mac Address" content={mac || ''} />
+        </ActionPanel>
+      }
+      key={interface$}
+      title={address || ''}
+      accessoryTitle={interface$}
+    ></List.Item>
+  )
+}
